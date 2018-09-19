fc89e47494fdb6c1d2dd1078cf22cfb62f2ca49f
Fanny Blandenet <fanny.blandenet@gmail.com>
2018-09-19 14:30:40 +0200

    Mise en place de la gestion des images

--- app/Picture.php
+++ app/Picture.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Picture extends Model
 {
+	protected $fillable = [
+        'title', 
+        'link',
+    ];
     public function post() {
     	return $this->belongsTo(Post::class);
     }
